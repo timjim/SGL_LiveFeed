68e7a1e5ede36f2b05d75fc45033c4d42ee26e32
Tim Elder <timothy.j.elder@gmail.com>
2014-07-01 11:55:47 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SglFeed::Application.config.secret_token = '6cb41666b183ef5908cfd77c23115fd92efa31439aa3f5c1260c1779806f12cbeb1ce6ebf8f8e198bd688ee775c081b622531af56f56c9a310a92351be6cdd89'
