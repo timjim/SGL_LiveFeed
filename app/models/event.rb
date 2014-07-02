cb3295ae4d60225125757f1bb8da5f4f27fd0265
Tim Elder <timothy.j.elder@gmail.com>
2014-07-02 13:49:21 +0100

    Time zone config for countdown

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
   attr_accessible :company, :company_image_url, :start_time, :end_time, :guest, :guest_bio, :guest_image_url, :host, :host_bio, :host_image_url, :location, :slido_url, :youtube_url, :eventbrite_url
 
-  scope :next_deadline, Proc.new { |after = DateTime.now, limit = 1| where('end_time > ?', after).order("end_time ASC").limit(limit) }
+  scope :next_deadline, Proc.new { |now = Time.zone.now, limit = 1| where('end_time > ?', now).order("end_time ASC").limit(limit) }
 end
