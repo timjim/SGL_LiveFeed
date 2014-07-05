21bdaa6e90f646e3e9742e300790dafe328a2ad1
Tim Elder <timothy.j.elder@gmail.com>
2014-07-05 18:48:14 +0100

    User controller override removed

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,6 @@ SglFeed::Application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
 
-  devise_scope :user do
-    resources :users
-  end
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
